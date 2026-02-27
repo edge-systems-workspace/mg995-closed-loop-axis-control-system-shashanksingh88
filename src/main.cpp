f35750e24df997d30f125ecef6387dde50cb27ea
g5n9u1-coder <shashank.singh2_cs.aiml@gla.ac.in>
2026-02-27 17:14:34 +0530

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -10,5 +10,9 @@ void loop() {
     myServo.write(0);    // Move to 0 degrees
     delay(1000);         // Wait 1 second
 
+    myServo.write(90);   // Move to 90 degrees
+    delay(1000);         // Wait 1 second
+
+
       // Wait 1 second
 }
